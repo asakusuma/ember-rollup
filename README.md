19f6a07b15ec5907545ac704619fe4df6b034099
asakusuma <asa.kusuma@gmail.com>
2016-10-12 16:08:30 -0700

    Namespace modules
    module ids are now namespaces on the addon name. This prevents
    collisions if multiple addons import different versions of the same
    module.
    
    Release 0.1.0 since this is a breaking change.

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # ember-rollup
-Use rollup to add runtime dependencies to an ember-cli build.
+Use rollup to add runtime dependencies to an ember-cli addon.
 
 ### What is this thing?
 
@@ -8,7 +8,7 @@ Use rollup to add runtime dependencies to an ember-cli build.
 * An array of string module names. These modules must be present in `dependencies` in `package.json`. Modules must define a `jsnext:main` property.
 * The original object to be exported in `index.js`
 
-Simply wrap the exports of your app/addon `index.js` with the function, and suddenly, ES6/2015 dependencies in your ember app! `ember-rollup` even handles [babel](http://babeljs.io/) for you.
+Simply wrap the exports of your app/addon `index.js` with the function, and suddenly, ES6/2015 dependencies in your ember app! `ember-rollup` even handles [babel](http://babeljs.io/) for you. Modules are namespaced based on the addon name.
 
 ### Example
 
@@ -25,6 +25,6 @@ module.exports = emberRollup(runtimeDependencies, {
 
 ```JavaScript
 //my-addon/app/my-thing.js
-import myModule from 'my-module';
-import RSVP from 'rsvp';
+import myModule from 'my-addon/my-module';
+import RSVP from 'my-addon/rsvp';
 ```
--- index.js
+++ index.js
@@ -6,6 +6,28 @@ var replace = require('broccoli-string-replace');
 var relative = require('require-relative');
 var path = require('path');
 
+function shouldAddRuntimeDependencies() {
+  var current = this;
+  var app;
+
+  // Keep iterating upward until we don't have a grandparent.
+  // Has to do this grandparent check because at some point we hit the project.
+  do {
+    app = current.app || app;
+  } while (current.parent.parent && (current = current.parent));
+
+  var isTopLevelAddon = false;
+  for (var i = 0; i < this.project.addons.length; i++) {
+    var addon = this.project.addons[i];
+    isTopLevelAddon = isTopLevelAddon || addon.name === this.name;
+  }
+
+  // If this addon isn't included directly by the app, all bets are off
+  // If the addon is included directly in the app, only import dependencies
+  // if this instance is the top level instance
+  return !isTopLevelAddon || !this.parent.parent;
+}
+
 module.exports = function(modules, indexObj) {
   var runtimeDependencies = modules.map(function(moduleName) {
     return {
@@ -14,94 +36,72 @@ module.exports = function(modules, indexObj) {
     };
   });
 
-  function treeForVendor(name) {
-    return merge(runtimeDependencies.map(function(dep) {
-      var main = require(dep.moduleName + '/package.json')['jsnext:main'];
-      var babelrcPath = path.dirname(main) + '/.babelrc';
+  function treeForAddon(root) {
+    if (shouldAddRuntimeDependencies.call(this)) {
+      var trees = runtimeDependencies.map(function(dep) {
+        var main = require(dep.moduleName + '/package.json')['jsnext:main'];
+        var babelrcPath = path.dirname(main) + '/.babelrc';
 
-      // Hacky way of getting the npm dependency folder
-      var depFolder = path.dirname(require.resolve(dep.moduleName + '/package.json'));
+        // Hacky way of getting the npm dependency folder
+        var depFolder = path.dirname(require.resolve(dep.moduleName + '/package.json'));
 
-      // Add the babelrc file
-      var babelRc = new Funnel(__dirname, {
-        include: ['rollup.babelrc'],
-        getDestinationPath: function(relativePath) {
-          if (relativePath === 'rollup.babelrc') {
-            return babelrcPath;
+        // Add the babelrc file
+        var babelRc = new Funnel(__dirname, {
+          include: ['rollup.babelrc'],
+          getDestinationPath: function(relativePath) {
+            if (relativePath === 'rollup.babelrc') {
+              return babelrcPath;
+            }
+            return relativePath;
           }
-          return relativePath;
-        }
+        });
+
+        var preset = path.dirname(relative.resolve('babel-preset-es2015/package.json', __dirname));
+
+        // Add an absolute path to the es2015 preset. Needed since host app
+        // won't have the preset
+        var mappedBabelRc = replace(babelRc, {
+          files: [ babelrcPath ],
+          pattern: {
+            match: /es2015/g,
+            replacement: preset
+          }
+        });
+
+        return new rollup(merge([
+          depFolder,
+          mappedBabelRc
+        ]), {
+          rollup: {
+            entry: main,
+            targets: [{
+              dest: dep.fileName,
+              format: 'es',
+              moduleId: dep.moduleName
+            }],
+            plugins: [
+              babel()
+            ]
+          }
+        });
       });
 
-      var preset = path.dirname(relative.resolve('babel-preset-es2015/package.json', __dirname));
-
-      // Add an absolute path to the es2015 preset. Needed since host app
-      // won't have the preset
-      var mappedBabelRc = replace(babelRc, {
-        files: [ babelrcPath ],
-        pattern: {
-          match: /es2015/g,
-          replacement: preset
-        }
-      });
-
-      return new rollup(merge([
-        depFolder,
-        mappedBabelRc
-      ]), {
-        rollup: {
-          entry: main,
-          targets: [{
-            dest: dep.fileName,
-            format: 'amd',
-            moduleId: dep.moduleName
-          }],
-          plugins: [
-            babel()
-          ]
-        }
-      });
-    }));
-  }
-
-  function included() {
-    this._super.included.apply(this, arguments);
-
-    var current = this;
-    var app;
-
-    // Keep iterating upward until we don't have a grandparent.
-    // Has to do this grandparent check because at some point we hit the project.
-    do {
-      app = current.app || app;
-    } while (current.parent.parent && (current = current.parent));
-
-    for (var i = 0; i < runtimeDependencies.length; i++) {
-      app.import('vendor/' + runtimeDependencies[i].fileName);
+      trees.push(root);
+      return this._super.treeForAddon.call(this, merge(trees.filter(Boolean)));
+    } else {
+      return this._super.treeForAddon.call(this, root);
     }
   }
 
-  if (indexObj.included) {
-    var old = indexObj.included;
-    indexObj.included = function() {
-      // Hack to ensure _super is declared
-      var _super = this._super;
-      old.apply(this, arguments);
-      included.apply(this, arguments);
-    }
-  } else {
-    indexObj.included = included;
-  }
-
-  if (indexObj.treeForVendor) {
-    indexObj.treeForVendor = function() {
+  if (indexObj.treeForAddon) {
+    indexObj.treeForAddon = function() {
       return merge([
-        indexObj.treeForVendor.apply(this, arguments),
-        treeForVendor.apply(this, arguments)
+        indexObj.treeForAddon.apply(this, arguments),
+        treeForAddon.apply(this, arguments)
       ]);
     }
   } else {
-    indexObj.treeForVendor = treeForVendor;
+    indexObj.treeForAddon = treeForAddon;
   }
 
   return indexObj;
